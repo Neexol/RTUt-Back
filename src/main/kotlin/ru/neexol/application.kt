2b80ff4a028fb0d987b9824e5f2e82d4b7303ddb
Nikita <almostroll@yandex.ru>
2022-04-21 18:36:24 +0300

    Initial commit

---
+++ src/main/kotlin/ru/neexol/application.kt
@@ -0,0 +1,21 @@
+package ru.neexol
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import ru.neexol.plugins.*
+
+fun main() {
+    embeddedServer(Netty) {
+        installCallLogging()
+        installContentNegotiation()
+
+        routing {
+            get("/") {
+                call.respondText("Hello!")
+            }
+        }
+    }.start(wait = true)
+}
