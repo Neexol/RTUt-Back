f5aecb7b8a81f8f37c42058ab69dbd3152263ebb
Nikita <almostroll@yandex.ru>
2022-04-29 20:48:16 +0300

    Add maps images as a static content

--- src/main/kotlin/ru/neexol/plugins/installRouting.kt
+++ src/main/kotlin/ru/neexol/plugins/installRouting.kt
@@ -3,9 +3,11 @@ package ru.neexol.plugins
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import ru.neexol.routes.apiRoute
+import ru.neexol.routes.staticRoute
 
 fun Application.installRouting() {
     install(Routing) {
         apiRoute()
+        staticRoute()
     }
 }
---
+++ src/main/kotlin/ru/neexol/routes/staticRoute.kt
@@ -0,0 +1,10 @@
+package ru.neexol.routes
+
+import io.ktor.server.http.content.*
+import io.ktor.server.routing.*
+
+fun Route.staticRoute() {
+    static("static") {
+        resources(".")
+    }
+}
